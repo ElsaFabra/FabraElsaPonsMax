a066212106f849c6a4231d0d6ca80a1d616d4319
ElsaFabra <efabrabe12@alumnes.ub.edu>
2026-03-02 22:21:33 +0100

    4t commit

--- src/prog2/model/Glamping.java
+++ src/prog2/model/Glamping.java
@@ -1,4 +1,39 @@
 package prog2.model;
 
 public class Glamping extends Casa{
+    private String material_;
+    private boolean casaMascota_;
+
+    public Glamping(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones, String material, boolean casaMascota){
+        super(nom_, idAllotjament_, mida, habitacions, placesPersones);
+        this.material_ = material;
+        this.casaMascota_ = casaMascota;
+        setEstadaMinima(3,3);
+    }
+
+    public String getMaterial_() {
+        return material_;
+    }
+
+    public void setMaterial_(String material) {
+        this.material_ = material;
+    }
+
+    public boolean isCasaMascota_() {
+        return casaMascota_;
+    }
+
+    public void setCasaMascota_(boolean casaMascota) {
+        this.casaMascota_ = casaMascota;
+    }
+
+    @Override
+    public boolean correcteFuncionament() {
+        return casaMascota_;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + ". Glamping(material  = " + material_ + ", casa de mascotes = " + casaMascota_ + ")";
+    }
 }
--- src/prog2/model/MobilHome.java
+++ src/prog2/model/MobilHome.java
@@ -1,4 +1,29 @@
 package prog2.model;
 
 public class MobilHome extends Casa{
+     private boolean terrassaBarbacoa_;
+
+    public MobilHome(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones, boolean terrassaBarbacoa) {
+        super(nom_, idAllotjament_, mida, habitacions, placesPersones);
+        this.terrassaBarbacoa_ = terrassaBarbacoa;
+        setEstadaMinima(3, 5);
+    }
+    public boolean isTerrassaBarbacoa_() {
+        return terrassaBarbacoa_;
+    }
+
+    public void setTerrassaBarbacoa_(boolean terrassaBarbacoa) {
+        this.terrassaBarbacoa_ = terrassaBarbacoa;
+    }
+
+    @Override
+    public boolean correcteFuncionament() {
+        return terrassaBarbacoa_;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ". Mobil-Home(Terrassa amb barbacoa = " + terrassaBarbacoa_ + ")";
+    }
+
 }
